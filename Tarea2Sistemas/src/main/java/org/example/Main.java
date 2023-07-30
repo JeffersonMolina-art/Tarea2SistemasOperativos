9a3357b6c8d0fa5e991e4968c2799b27aef5af4b
JeffersonMolina-art <84147110+JeffersonMolina-art@users.noreply.github.com>
2023-07-30 17:15:52 -0600

    Quinta version

--- Tarea2Sistemas/src/main/java/org/example/Main.java
+++ Tarea2Sistemas/src/main/java/org/example/Main.java
@@ -7,15 +7,18 @@ public class Main {
         Scanner d = new Scanner(System.in);
         String name;
 
-        System.out.println("cual es tu nombre");
+        System.out.println("Cual es tu nombre");
         name =  d.nextLine();
-        System.out.println("cual es tu edad");
+        System.out.println("Cual es tu edad");
         int edad =  d.nextInt();
         if(edad >= 18){
-            System.out.println("Hola "+ name + "cuentas con la edad de "+edad + "eres mayor de edad");
+            System.out.println("Realiza tu pedido de comida");
+            String pedido = d.nextLine();
+            System.out.println("Hola "+ name + " cuentas con la edad de "+edad + "eres mayor de edad"+" Tu pedido de: "+pedido+ " estara listo en breve");
         }else{
-            System.out.println("Hola "+ name + "cuentas con la edad de "+edad + "eres menor de edad");
+            System.out.println("Hola "+ name + " cuentas con la edad de "+edad + " eres menor de edad no puedes ordenar comida");
         }
+        System.out.println("");
 
     }
 }
